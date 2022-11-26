90d80f87e23e1bf64f8d012809a4ea27a401c3c7
S3R43o3 <67480273+sera619@users.noreply.github.com>
2022-11-26 09:58:57 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,169 @@
+from kivy.lang import Builder
+from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel
+
+
+
+class GameApp(MDApp):
+    def build(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Orange"
+        return Builder.load_file('layout.kv')
+    
+        
+    # define whos turn it is
+    turn = "X"
+    # track win or lose
+    winner = False
+
+    def presser(self, btn):
+        if self.turn == "X":
+            btn.color = 1 , 0 , 0 , 1
+            btn.background_color = 1 , 0 , 0 , .2
+            btn.text = "X"
+            self.turn = "O"
+            self.root.ids.score.text = "O´s Turn!"
+            btn.disabled = True
+        else:
+            btn.color = 0 , 0 , 1 , 1
+            btn.background_color = 0 , 0 , 1 , .2
+            btn.text = "O"
+            self.turn = "X"
+            self.root.ids.score.text = "X´s Turn!"
+            btn.disabled = True
+
+        self.win()
+
+    def win(self):
+       # Across
+        if self.root.ids.btn1.text != "" and self.root.ids.btn1.text == self.root.ids.btn2.text and self.root.ids.btn2.text == self.root.ids.btn3.text:
+            return self.end_game(self.root.ids.btn1, self.root.ids.btn2, self.root.ids.btn3)
+
+        if self.root.ids.btn4.text != "" and self.root.ids.btn4.text == self.root.ids.btn5.text and self.root.ids.btn5.text == self.root.ids.btn6.text:
+            return self.end_game(self.root.ids.btn4, self.root.ids.btn5, self.root.ids.btn6)
+
+        if self.root.ids.btn7.text != "" and self.root.ids.btn7.text == self.root.ids.btn8.text and self.root.ids.btn8.text == self.root.ids.btn9.text:
+            return self.end_game(self.root.ids.btn7, self.root.ids.btn8, self.root.ids.btn9)
+
+        # Down
+        if self.root.ids.btn1.text != "" and self.root.ids.btn1.text == self.root.ids.btn4.text and self.root.ids.btn4.text == self.root.ids.btn7.text:
+            return self.end_game(self.root.ids.btn1, self.root.ids.btn4, self.root.ids.btn7)
+
+        if self.root.ids.btn2.text != "" and self.root.ids.btn2.text == self.root.ids.btn5.text and self.root.ids.btn5.text == self.root.ids.btn8.text:
+            return self.end_game(self.root.ids.btn2, self.root.ids.btn5, self.root.ids.btn8)
+
+        if self.root.ids.btn3.text != "" and self.root.ids.btn3.text == self.root.ids.btn6.text and self.root.ids.btn6.text == self.root.ids.btn9.text:
+            return self.end_game(self.root.ids.btn3, self.root.ids.btn6, self.root.ids.btn9)
+
+        # Diagonal 
+        if self.root.ids.btn1.text != "" and self.root.ids.btn1.text == self.root.ids.btn5.text and self.root.ids.btn5.text == self.root.ids.btn9.text:
+            return self.end_game(self.root.ids.btn1, self.root.ids.btn5, self.root.ids.btn9)
+
+        if self.root.ids.btn3.text != "" and self.root.ids.btn3.text == self.root.ids.btn5.text and self.root.ids.btn5.text == self.root.ids.btn7.text:
+            return self.end_game(self.root.ids.btn3, self.root.ids.btn5, self.root.ids.btn7)
+
+        self.no_winner()
+
+    def no_winner(self):
+        if self.winner == False and \
+        self.root.ids.btn1.disabled == True and \
+        self.root.ids.btn2.disabled == True and \
+        self.root.ids.btn3.disabled == True and \
+        self.root.ids.btn4.disabled == True and \
+        self.root.ids.btn5.disabled == True and \
+        self.root.ids.btn6.disabled == True and \
+        self.root.ids.btn7.disabled == True and \
+        self.root.ids.btn8.disabled == True and \
+        self.root.ids.btn9.disabled == True:
+            self.root.ids.score.text = "IT'S A TIE!!"
+
+    def disable_all_buttons(self):
+        self.root.ids.btn1.disabled = True
+        self.root.ids.btn2.disabled = True
+        self.root.ids.btn3.disabled = True
+        self.root.ids.btn4.disabled = True
+        self.root.ids.btn5.disabled = True
+        self.root.ids.btn6.disabled = True
+        self.root.ids.btn7.disabled = True
+        self.root.ids.btn8.disabled = True
+        self.root.ids.btn9.disabled = True
+    
+    def reset_btn_colors(self):
+        self.root.ids.btn1.color = "green"
+        self.root.ids.btn2.color = "green"
+        self.root.ids.btn3.color = "green"
+        self.root.ids.btn4.color = "green"
+        self.root.ids.btn5.color = "green"
+        self.root.ids.btn6.color = "green"
+        self.root.ids.btn7.color = "green"
+        self.root.ids.btn8.color = "green"
+        self.root.ids.btn9.color = "green"
+
+        self.root.ids.btn1.background_color = "green"
+        self.root.ids.btn2.background_color = "green"
+        self.root.ids.btn3.background_color = "green"
+        self.root.ids.btn4.background_color = "green"
+        self.root.ids.btn5.background_color = "green"
+        self.root.ids.btn6.background_color = "green"
+        self.root.ids.btn7.background_color = "green"
+        self.root.ids.btn8.background_color = "green"
+        self.root.ids.btn9.background_color = "green"
+
+    def restart(self):
+        # reset btn colors
+        self.reset_btn_colors()
+        
+        # enable all buttons
+        self.root.ids.btn1.disabled = False
+        self.root.ids.btn2.disabled = False
+        self.root.ids.btn3.disabled = False
+        self.root.ids.btn4.disabled = False
+        self.root.ids.btn5.disabled = False
+        self.root.ids.btn6.disabled = False
+        self.root.ids.btn7.disabled = False
+        self.root.ids.btn8.disabled = False
+        self.root.ids.btn9.disabled = False
+
+        # clear all buttons
+        self.root.ids.btn1.text =""
+        self.root.ids.btn2.text =""
+        self.root.ids.btn3.text =""
+        self.root.ids.btn4.text =""
+        self.root.ids.btn5.text =""
+        self.root.ids.btn6.text =""
+        self.root.ids.btn7.text =""
+        self.root.ids.btn8.text =""
+        self.root.ids.btn9.text =""
+
+        # reset score text
+        self.root.ids.score.color = "white"
+
+        self.root.ids.score.text = "X goes first!"
+
+        # reset winner
+        self.winner = False
+
+        # reset turn
+        self.turn = "X"
+    
+    def end_game(self, a, b, c):
+        self.winner = True
+        a.color = "red"
+        b.color = "red"
+        c.color = "red"
+        self.disable_all_buttons()
+        self.root.ids.score.color = "green"
+        self.root.ids.score.text = f"{a.text} Wins!"
+    
+    def exit_game(self):
+        app.stop()
+        exit(0)
+
+
+if __name__=="__main__":
+    app = GameApp()
+    try:
+        app.run()
+    except (KeyboardInterrupt, SystemExit):
+        print("Kivy app closed")
+        exit(0)
